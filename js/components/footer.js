d25d52fb46b5135e13b7e7cbb98e18f68da6c833
Bastian Brisso <166576401+Brissox@users.noreply.github.com>
2025-09-02 16:21:31 +0000

    Modificaciones base

---
+++ js/components/footer.js
@@ -0,0 +1,5 @@
+document.getElementById("footer").innerHTML = `   
+<footer class="bg-dark text-white py-3">
+    <p class="mb-0">2025 Ventas Duoc UC - Todos los derechos reservados</p>
+</footer>
+`;
