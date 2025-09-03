c5dc72242e8ab4e2c86ef81598424d757a816284
Bastian Brisso <166576401+Brissox@users.noreply.github.com>
2025-09-03 16:09:51 +0000

    footer

--- js/components/footer.js
+++ js/components/footer.js
@@ -1,5 +1,61 @@
 document.getElementById("footer").innerHTML = `   
-<footer class="bg-dark text-white py-3">
-    <p class="mb-0">2025 B&regrisso Duoc UC - Todos los derechos reservados</p>
+
+<footer>
+  <div class="footer-content bg-black pt-[40px] pb-4 text-white">
+    <section class="footer-nav max-w-[1124px] mx-auto flex flex-col lg:flex-row lg:justify-between">
+      
+      <!-- Navegación -->
+      <nav class="lg:flex-1">
+        <ul>
+          <li class="mb-2">
+            <a class="text-white text-sm hover:underline" href="#" target="_blank" rel="noopener noreferrer">
+              Cómo modificar mis datos
+            </a>
+          </li>
+	<li class="mb-2">
+            <a class="text-white text-sm hover:underline" href="#" target="_blank" rel="noopener noreferrer">
+              Cómo modificar mis datos
+            </a>
+          </li>
+	<li class="mb-2">
+            <a class="text-white text-sm hover:underline" href="#" target="_blank" rel="noopener noreferrer">
+              Cómo modificar mis datos
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <!-- Lado derecho con imagen e íconos -->
+      <div class="footer-aside lg:flex-1 lg:border-l lg:border-gray-700 lg:pl-6 mt-6 lg:mt-0">
+        <!-- Imagen -->
+        <img class="mb-4 w-[120px] h-auto" src="https://img.freepik.com/vector-premium/logotipo-pasteleria-fondo-blanco_1197524-14327.jpg" alt="Jumbo Prime">
+
+        <!-- Íconos sociales -->
+       <div class="flex gap-4 text-[24px] text-gray-400">
+  <a href="#" target="_blank" aria-label="Instagram">
+    <i class="bi bi-instagram hover:text-white transition-colors"></i>
+  </a>
+  <a href="#" target="_blank" aria-label="Facebook">
+    <i class="bi bi-facebook hover:text-white transition-colors"></i>
+  </a>
+  <a href="#" target="_blank" aria-label="TikTok">
+    <i class="bi bi-tiktok hover:text-white transition-colors"></i>
+  </a>
+  <a href="#" target="_blank" aria-label="YouTube">
+    <i class="bi bi-youtube hover:text-white transition-colors"></i>
+  </a>
+</div
+      </div>
+      
+    </section>
+  </div>
 </footer>
+
+    </section><div class="footer-copyright py-1 px-0 my-0 m-auto lg:max-w-[1440px]">
+        <div
+            class="footer-copyright-content w-full text-center text-[14px] flex flex-col items-center justify-center font-normal lg:flex-row lg:m-auto">
+ 	<p class="mb-1 text-Black">2025 B&regrisso Duoc UC - Todos los derechos reservados</p>     
+    </div>
+</div>
+
 `;
